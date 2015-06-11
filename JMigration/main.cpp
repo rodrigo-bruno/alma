5c24e50392e053a43f5c4176af39db7d6790233e
rodrigo-bruno <rodrigo.bruno@ist.utl.pt>
2015-06-11 17:57:15 +0100

    Initial commit for the JMigration repo.

---
+++ JMigration/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: underscore
+ *
+ * Check: http://www.javaadvent.com/2014/12/own-your-heap-iterate-class-instances.html
+ * Check: http://www.javaadvent.com/2014/12/own-your-heap-iterate-class-instances.html
+ * Created on June 11, 2015, 5:53 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
