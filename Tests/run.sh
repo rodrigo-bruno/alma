023e035c4a67aa1fdca71467b94862a425e0486f
rodrigo-bruno <rodrigo.bruno@ist.utl.pt>
2015-06-15 18:10:24 +0100

    Now using my openjdk built.

--- Tests/run.sh
+++ Tests/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-agent_path=/home/underscore/git/JMigration/JMigration/dist/Debug/GNU-Linux-x86/libJMigration.so
 
-javac $1/Main.java
-java -cp $1 -XX:+UseG1GC -agentpath:$agent_path Main
+# Usage: ./run.sh <directory with Main.java>
+
+agent_path=/home/underscore/git/JMigration/JMigration/dist/Debug/GNU-Linux-x86/libJMigration.so
+openjdk=/home/underscore/mercurial/jdk8/build/linux-x86_64-normal-server-release/jdk/bin
+javac=$openjdk/javac
+java=$openjdk/java
+
+$javac $1/Main.java
+$java -cp $1 -XX:+UseG1GC -agentpath:$agent_path Main
